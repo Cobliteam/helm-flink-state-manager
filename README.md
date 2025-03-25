e19f1da83d86df96eac501db31f3ec953bef22e4
BrunoBBS <brunobscholl@gmail.com>
2025-03-25 10:57:54 -0300

    Add all files
    Signed-off-by: BrunoBBS <brunobscholl@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # helm-flink-state-manager
+
+The name of the repo describes the plugin, but the name of the plugin is simpler
+as it is the helm command.
---
+++ flink-state-manager.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+original_args=("$@")
+
+VALUES_FILES=()
+CONTEXT="cobli-prod-devices"
+CHART_PATH=$2
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --values|-f)
+      VALUES_FILES+=("$2")
+      shift 2
+      ;;
+    --kube-context)
+      CONTEXT+=("$2")
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+OUTPUT=$(mktemp)
+npx @cobliteam/flink-state-manager@1.1.1 --values-path $CHART_PATH/values.yaml --prod-values-path ${VALUES_FILES[0]} --context $CONTEXT --menu | tee $OUTPUT
+
+cat $OUTPUT
+
+# Create a temporary values file
+TMP_VALUES=$(mktemp)
+echo "
+alex-job-chart:
+  jobUpgradeMode: savepoint
+  jobRestoreFromSavepoint:
+    enabled: true
+    savepointPath: $(tail -n 1 $OUTPUT| cut -d ':' -f 2)
+" > "$TMP_VALUES"
+
+# Add your values file to the command
+helm upgrade --install ${original_args[@]} --values $TMP_VALUES > aaa.yaml
