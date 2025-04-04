00ac93094ae7833aee94ab63ca8d25a9fe71ca93
BrunoBBS <brunobscholl@gmail.com>
2025-04-04 15:42:02 -0300

    fix: Correctly pass namespace and context to state-manager
    Signed-off-by: BrunoBBS <brunobscholl@gmail.com>

--- flink-state-manager.sh
+++ flink-state-manager.sh
@@ -5,17 +5,12 @@ set -o pipefail
 original_args=("$@")
 
 VALUES_FILES=()
-CONTEXT="cobli-prod-devices"
 CHART_PATH=$2
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --values|-f)
       VALUES_FILES+=("$2")
-      shift 2
-      ;;
-    --kube-context)
-      CONTEXT="$2"
       shift 2
       ;;
     *)
@@ -25,7 +20,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 OUTPUT=$(mktemp)
-npx @cobliteam/flink-state-manager@1.1.1 --values-path "$CHART_PATH"/values.yaml --prod-values-path "${VALUES_FILES[0]}" --context "$CONTEXT" --menu | tee "$OUTPUT"
+npx @cobliteam/flink-state-manager@1.1.1 --values-path "$CHART_PATH"/values.yaml --prod-values-path "${VALUES_FILES[0]}" --context "$HELM_KUBECONTEXT" --menu | tee "$OUTPUT"
 
 cat "$OUTPUT"
 
@@ -40,4 +35,5 @@ alex-job-chart:
 " > "$TMP_VALUES"
 
 # Add your values file to the command
-helm upgrade --install "${original_args[@]}" --values "$TMP_VALUES"
+echo "Executing ->  helm upgrade --install "${original_args[@]}" --namespace $HELM_NAMESPACE --kube-context $HELM_KUBECONTEXT --values "$TMP_VALUES""
+helm upgrade --install "${original_args[@]}" --namespace $HELM_NAMESPACE --kube-context $HELM_KUBECONTEXT --values "$TMP_VALUES"
