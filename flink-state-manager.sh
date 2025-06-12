bd1b0628981c7caa7a1d15b83e126697b62b2297
BrunoBBS <brunobscholl@gmail.com>
2025-06-12 11:31:24 -0300

    feat: Add checkpoint restoration
    Signed-off-by: BrunoBBS <brunobscholl@gmail.com>

--- flink-state-manager.sh
+++ flink-state-manager.sh
@@ -19,10 +19,29 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-OUTPUT=$(mktemp)
-npx @cobliteam/flink-state-manager@1.2.4 --values-path "$CHART_PATH"/values.yaml --prod-values-path "${VALUES_FILES[0]}" --context "$HELM_KUBECONTEXT" --menu | tee "$OUTPUT"
+# Ask user whether to restore from a checkpoint or a savepoint using a selector
 
-# Create a temporary values file
+PS3="Select restore mode: "
+select RESTORE_MODE in "From a checkpoint" "From a savepoint"; do
+  case $RESTORE_MODE in
+    "From a checkpoint"| "From a savepoint")
+      break
+      ;;
+    *)
+      echo "Invalid option. Please select 1 or 2."
+      ;;
+  esac
+ done
+
+OUTPUT=$(mktemp)
+if [[ "$RESTORE_MODE" == "From a checkpoint" ]]; then
+  npx @cobliteam/flink-state-manager@1.4.0 --values-path "$CHART_PATH"/values.yaml --prod-values-path "${VALUES_FILES[0]}" --context "$HELM_KUBECONTEXT" --menu --checkpoints | tee "$OUTPUT"
+else
+  npx @cobliteam/flink-state-manager@1.4.0 --values-path "$CHART_PATH"/values.yaml --prod-values-path "${VALUES_FILES[0]}" --context "$HELM_KUBECONTEXT" --menu | tee "$OUTPUT"
+fi
+
+# Create a temporary values file. This entry can receive either a savepoint or a
+# checkpoint path.
 TMP_VALUES=$(mktemp)
 echo "
 alex-job-chart:
@@ -34,5 +53,5 @@ alex-job-chart:
 
 # Add your values file to the command
 echo
-echo "helm upgrade --install "${original_args[@]}" --namespace $HELM_NAMESPACE --kube-context $HELM_KUBECONTEXT --values "$TMP_VALUES""
+echo "helm upgrade --install \"${original_args[@]}\" --namespace $HELM_NAMESPACE --kube-context $HELM_KUBECONTEXT --values \"$TMP_VALUES\""
 helm upgrade --install "${original_args[@]}" --namespace $HELM_NAMESPACE --kube-context $HELM_KUBECONTEXT --values "$TMP_VALUES"
